2aca5812548653b10885d1d2fb7ce7e316ec8a95
Shane Boyce <smcgb1844@gmail.com>
2026-01-10 10:06:58 -0800

    Initial upload of files

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import *
+from logger import log_state
+from player import Player
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player( x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    clock = pygame.time.Clock()
+    dt=0
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+        dt += clock.get_time() / 1000
+
+if __name__ == "__main__":
+    main()
