71921bd35cb88bad96df4282d7494f43752d331a
Shane Boyce <smcgb1844@gmail.com>
2026-01-14 19:28:10 -0800

    Feature: Shot class

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -21,9 +22,11 @@ def main():
     asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group() # objects that can be updates
     drawable = pygame.sprite.Group()   # objects that can be drawn on screen
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
     # screen and player
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player( x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
